2565189d9699e5e7812d8ac69a90498c96dfe304
3abdoo <abdelrahmantarek1616@gmail.com>
2025-12-11 23:19:53 +0200

    Update Subject.java

--- src/models/Subject.java
+++ src/models/Subject.java
@@ -1,5 +1,18 @@
 package models;
 
 public class Subject {
-    
+     protected String subid;
+     protected String subname;
+    public String getSubid() {
+        return subid;
+    }
+    public void setSubid(String subid) {
+        this.subid = subid;
+    }
+    public String getSubname() {
+        return subname;
+    }
+    public void setSubname(String subname) {
+        this.subname = subname;
+    }
 }
