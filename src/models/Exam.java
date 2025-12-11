ebca001b74252c55aad4e22c64fb352d40e50d0e
mekkyy0 <ziadmekky00@gmail.com>
2025-12-11 15:05:45 +0200

    student-1

--- src/models/Exam.java
+++ src/models/Exam.java
@@ -1,5 +1,13 @@
 package models;
 
 public class Exam {
-    
+    public String examId;
+    public String subjectName;
+    public int duration;
+
+    public Exam(String id, String sub, int d) {
+        examId = id;
+        subjectName = sub;
+        duration = d;
+    }
 }
