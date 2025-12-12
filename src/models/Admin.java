1765a8126e96c7abc7ead6ecda73b4ed21cb7a31
AbdullahHAboElkeir <bedohisham234@gmail.com>
2025-12-12 18:17:30 +0200

    add
    add

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -1,12 +1,17 @@
 package models;
-
 public class Admin extends User {
 
-    private String username;
-
-    public Admin(int id, String name, String email, String password, String username) {
-        super(id, name, email, password);
-        this.username = username;
+    public Admin(int id, String name, String email, String password) {
+        super(id, name, email, password, "admin");
     }
 
+    public void manageUsers() {
+        System.out.println("Admin can manage users.");
+    }
+
+    @Override
+    public boolean login(String email, String password ) {
+        System.out.println("Admin login attempt...");
+        return super.login(email, password);
+    }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,23 +1,23 @@
 package models;
 
 public class User {
-    protected  int  id;
+    protected int id;
     protected String name;
     protected String email;
     protected String password;
+    protected String role;
 
-    public User(int id, String name, String email, String username, String password) {}
-
-    public User(int id, String name, String email, String password) {
+    public User(int id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.role = role;
     }
 
-
+    // Getters & Setters
     public int getId() { return id; }
-    public void setId(int  id) { this.id = id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -28,10 +28,20 @@ public class User {
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 
     public boolean login(String email, String password) {
         return this.email.equals(email) && this.password.equals(password);
     }
 
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
